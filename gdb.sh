aa47f2eecfa76aaaa83aedefc018daa867e3a039
Eddie Xiao <thy.isnis@gmail.com>
2024-10-23 19:57:43 -0400

    Updates gdb and run to support two kernels

--- gdb.sh
+++ gdb.sh
@@ -1,2 +1,1 @@
-# gdb -x gdbscript --se="x64/build/kernel64" --se="x32/build/kernel32"
-gdb -x gdbscript  --se="x32/build/kernel32"
+gdb -x gdbscript --se="x32/build/kernel32" --se="x64/build/kernel64"

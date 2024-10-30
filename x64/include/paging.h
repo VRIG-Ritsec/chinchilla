b8d5fd72da992f3b60b2231ddc4b0cff06991030
Eddie Xiao <thy.isnis@gmail.com>
2024-10-30 13:49:00 -0400

    Creates macros for pfn, page,phys conversions

--- x64/include/paging.h
+++ x64/include/paging.h
@@ -16,4 +16,20 @@ struct __attribute__((aligned(32))) page_struct{
     struct list_head list;
 };
 
+#define SYSTEM_MAX_MEMORY (4ul * GIGABYTE)   // our system only supports 4GB of memory
+#define PAGE_STRUCTS_NEEDED (SYSTEM_MAX_MEMORY >> PAGE_SHIFT)
+#define PAGE_ARRAY_LEN (PAGE_STRUCTS_NEEDED * sizeof(struct page_struct))
+
+extern struct page_struct page_array[PAGE_ARRAY_LEN]; //we support up to 4G of memory
+
+#define PFN_TO_PHYS(pfn) (pfn << PAGE_SHIFT)
+#define PFN_TO_PAGE(pfn) (&page_array[pfn])
+
+
+#define PAGE_TO_PFN(page) ((struct page_struct * )page - &page_array)
+#define PAGE_TO_PHYS(page) (PFN_TO_PHYS(PAGE_TO_PFN(page)))
+
+#define PHYS_TO_PFN(addr) (addr >> PAGE_SHIFT)
+#define PHYS_TO_PAGE(addr) (PFN_TO_PAGE(PHYS_TO_PFN(addr)))
+
 #endif

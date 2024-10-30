8ea17bf4bda3f71a31a18721333f9e13909d2a34
Eddie Xiao <thy.isnis@gmail.com>
2024-10-30 14:38:28 -0400

    BUG: Can't compile free_area incompelte type

--- x64/src/buddy.c
+++ x64/src/buddy.c
@@ -1,6 +1,20 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include "list.h"
+#include "paging.h"
 #include "printf.h"
 #include "multiboot.h"
 #include "kernel32.h"
 #include "buddy.h"
+
+
+struct free_area free_area = {};
+
+// store the start and end of kernel image so we dont' accidently free these pages
+extern uint64_t kernel64_start;
+extern uint64_t kernel64_end;
+
+// function takes a list head and extracts the page_struct out of it
+static inline struct page_struct * list_page(struct list_head * list){
+    return list_entry(list, struct page_struct, list);
+}

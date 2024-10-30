07740e1e155425a60a8c1628742207611054bf8f
Eddie Xiao <thy.isnis@gmail.com>
2024-10-30 14:38:12 -0400

    Creates free_area struct

--- x64/include/buddy.h
+++ x64/include/buddy.h
@@ -1,3 +1,21 @@
 #ifndef __BUDDY_H
 #define __BUDDY_H
+
+#include "./list.h"
+#include "./types.h"
+
+
+/*#define ORDER_TO_SIZE(order) (PAGE_SIZE * 2 ** order)*/
+#define ORDER_TO_SIZE (1 << (order + PAGE_SHIFT))  // same as above but uses more efficent bit shift math
+
+
+
+// Max order of buddy sizes is 11, so max size if 0x1000 * 2 ** 11
+#define MAX_ORDER 11
+struct free_area{
+    struct list_head orders[MAX_ORDER+1];
+};
+
+// invoked as a pointer as functions that use free_area as a function name overwrite the global free_area struct
+#define GET_ORDER_HEAD(order) (free_area->orders[order])
 #endif

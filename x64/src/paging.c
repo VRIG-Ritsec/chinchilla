58bd7532465bcdad6156487e7dd4e4cfa10bc6eb
Eddie Xiao <thy.isnis@gmail.com>
2024-11-11 00:15:48 -0500

    Initalizes 4GB page table entries in 64-bit mode

--- x64/src/paging.c
+++ x64/src/paging.c
@@ -6,9 +6,9 @@ extern u64 kernel_pud[PAGE_DESC_COUNT];
 
 // setup block addressing 
 void init_64_bit_page_tables(){
-    /*kernel_pgd[0] = MK_PGD_ENTRY(&kernel_pud);*/
-    /*// loops per each gig*/
-    /*for(u32 i = 0; i < 4; i++){*/
-    /*    kernel_pud[i] = MK_PUD_ENTRY(i * GIGABYTE);*/
-    /*}*/
+    kernel_pgd[0] = PGD_ENTRY((u64)&kernel_pud);
+    // loops per each gig
+    for(u32 i = 0; i < 4; i++){
+        kernel_pud[i] = PUD_ENTRY(i * GIGABYTE);
+    }
 }

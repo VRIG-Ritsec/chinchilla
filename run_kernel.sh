997c7e6317e5c33961f18dfe540eeee77c07f98f
Eddie Xiao <thy.isnis@gmail.com>
2024-11-10 23:38:24 -0500

    Set's QEMU RAM to 4GB to fix paging issue

--- run_kernel.sh
+++ run_kernel.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-qemu-system-x86_64 -m 4G -cpu host -kernel x32/build/kernel32  -device loader,file="x64/build/kernel64" -serial stdio -enable-kvm -nographic -monitor null -s -S 
+qemu-system-x86_64  -cpu host -kernel x32/build/kernel32  -device loader,file="x64/build/kernel64" -serial stdio -enable-kvm -nographic -monitor null -s -S 

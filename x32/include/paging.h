caf258f64ed5f6ca7645d6c0aec9a6ad365d091f
Eddie Xiao <thy.isnis@gmail.com>
2024-11-10 23:30:43 -0500

    Adds block addressing

--- x32/include/paging.h
+++ x32/include/paging.h
@@ -5,6 +5,7 @@ void * init_page_table();
 
 #define PG_PRESENT 1
 #define PG_WRITE (1<<1)
+#define PG_PS (1 << 7)  // used for block addressing 
 
 #define PAGE_SIZE 0x1000
 #define TWO_M (PAGE_SIZE * 0x200)

d1cbfc094198217c02b81b9c100f7a7ada61b337
Eddie Xiao <thy.isnis@gmail.com>
2024-11-11 22:32:04 -0500

    Updates assert macro with printf vargs support

--- x64/include/assert.h
+++ x64/include/assert.h
@@ -1,9 +1,9 @@
 #ifndef __ASSERT_H
 #define __ASSERT_H
 
-#define ASSERT(condition, err_msg) ({\
+#define ASSERT(condition, err_msg, ...) ({\
 if(condition){\
-    printf("===================================ERROR===================================\n%s\n", err_msg);\
+    printf("===================================ERROR===================================\nBug @ %s:%d\n%s\n", err_msg, __FILE__, __LINE__, ##__VA_ARGS__);\
     __asm__("hlt");\
 }})\
 

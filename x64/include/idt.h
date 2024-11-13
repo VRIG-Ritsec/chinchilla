e1bcf6d2788c7a21cb10ac925a717fc8d1c7fb25
Eddie Xiao <thy.isnis@gmail.com>
2024-11-13 18:15:27 -0500

    Adds idt structures and builds test trap entry

--- x64/include/idt.h
+++ x64/include/idt.h
@@ -1,30 +1,29 @@
 /*
- * include/idt.h 
+ * include/idt.h
  * Interrupt Descriptor Table structures
-*/
+ */
 
 #ifndef __IDT_H
 #define __IDT_H
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
-#include "types.h"
-#include "paging.h"
 #include "list.h"
+#include "paging.h"
+#include "types.h"
 
-#include "printf.h"
-#include "multiboot.h"
+#include "assert.h"
 #include "kernel32.h"
-#include "assert.h"
 #include "math.h"
+#include "multiboot.h"
+#include "printf.h"
 
 #include "assert.h"
+#include "buddy.h"
 #include "kernel.h"
 #include "list.h"
 #include "paging.h"
-#include "buddy.h"
-
 
 // Various bits that the IDT requires
 #define IDT_PRESENT (1UL << 7)
@@ -32,13 +31,28 @@
 #define IDT_INTR_GATE 0xe
 
 // Copied from https://wiki.osdev.org/Interrupt_Descriptor_Table
-struct idt64{
-   uint16_t offset_1;        // offset bits 0..15
-   uint16_t selector;        // a code segment selector in GDT or LDT
-   uint8_t  ist;             // bits 0..2 holds Interrupt Stack Table offset, rest of bits zero.
-   uint8_t  type_attributes; // gate type, dpl, and p fields
-   uint16_t offset_2;        // offset bits 16..31
-   uint32_t offset_3;        // offset bits 32..63
-   uint32_t zero;            // reserved
+struct idt64 {
+    uint16_t offset_1; // offset bits 0..15
+    uint16_t selector; // a code segment selector in GDT or LDT
+    uint8_t
+        ist; // bits 0..2 holds Interrupt Stack Table offset, rest of bits zero.
+    uint8_t type_attributes; // gate type, dpl, and p fields
+    uint16_t offset_2;       // offset bits 16..31
+    uint32_t offset_3;       // offset bits 32..63
+    uint32_t zero;           // reserved
 };
+
+#define IDT_ENTRY(offset, attribute)                                           \
+    {.offset_1 = (offset) & 0xffff,                                            \
+     .selector = 1,                                                            \
+     .ist = 0,                                                                 \
+     .type_attributes = IDT_PRESENT | attribute,                               \
+     .offset_2 = ((offset) >> 16) & 0xffff,                                    \
+     .offset_3 = ((offset) >> 32),                                             \
+     .zero = 0}
+
+// @TODO make macro for both these fuckers
+#define IDT_TRAP_ENTRY(offset) IDT_ENTRY(offset, IDT_TRAP_GATE)
+
+#define IDT_INTR_ENTRY(offset) IDT_ENTRY(offset, IDT_INTR_GATE)
 #endif
--- x64/src/helper.s
+++ x64/src/helper.s
@@ -13,6 +13,6 @@ inb:
 	in al, dx
 	ret
 
-// loads value into the lidt register
-global lidt
-lidt:
+;loads value into the lidt register
+;global lidt
+;lidt:

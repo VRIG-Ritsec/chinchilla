859a13426f8621b720cb23d9724c9f1e5c461b5c
Eddie Xiao <thy.isnis@gmail.com>
2024-11-13 18:51:00 -0500

    Adds indexs for IDT

--- x64/include/idt.h
+++ x64/include/idt.h
@@ -1,6 +1,6 @@
 /*
  * include/idt.h 
- * Interrupt Descriptor Table structures
+ * Interrupt Descriptor Table structures and values
 */
 
 #ifndef __IDT_H
@@ -47,5 +47,33 @@ struct idtr{
     u16 size; 
     u64 offset;
 };
+
+// Exception Vector Numbers, used to index the Interrupt Descriptor Table
+// https://wiki.osdev.org/Exceptions
+#define IDT_DIVIDE_ERROR               0    // Divide-by-zero Error
+#define IDT_DEBUG                      1    // Debug Exception
+#define IDT_NMI_INTERRUPT              2    // Non-maskable Interrupt
+#define IDT_BREAKPOINT                 3    // Breakpoint (INT3)
+#define IDT_OVERFLOW                   4    // Overflow (INTO instruction)
+#define IDT_BOUND_RANGE_EXCEEDED       5    // Bound Range Exceeded (BOUND instruction)
+#define IDT_INVALID_OPCODE             6    // Invalid Opcode (Undefined Opcode)
+#define IDT_DEVICE_NOT_AVAILABLE       7    // Device Not Available (No Math Coprocessor)
+#define IDT_DOUBLE_FAULT               8    // Double Fault
+#define IDT_COPROCESSOR_SEGMENT_OVERRUN 9   // Coprocessor Segment Overrun (reserved, no longer used)
+#define IDT_INVALID_TSS                10   // Invalid TSS
+#define IDT_SEGMENT_NOT_PRESENT        11   // Segment Not Present
+#define IDT_STACK_SEGMENT_FAULT        12   // Stack-Segment Fault
+#define IDT_GENERAL_PROTECTION_FAULT   13   // General Protection Fault
+#define IDT_PAGE_FAULT                 14   // Page Fault
+#define IDT_RESERVED                   15   // Reserved (Intel-reserved, do not use)
+#define IDT_X87_FLOATING_POINT_ERROR   16   // x87 Floating-Point Error (Math Fault)
+#define IDT_ALIGNMENT_CHECK            17   // Alignment Check
+#define IDT_MACHINE_CHECK              18   // Machine Check
+#define IDT_SIMD_FLOATING_POINT_ERROR  19   // SIMD Floating-Point Exception
+#define IDT_VIRTUALIZATION_EXCEPTION   20   // Virtualization Exception
+#define IDT_CONTROL_PROTECTION_EXCEPTION 21 // Control Protection Exception (Intel CET)
+
+#define IDT_EXCEPTION_RESERVED_MIN     22   // Reserved exception vectors 22–31
+#define IDT_EXCEPTION_RESERVED_MAX     31   // Last reserved exception vector
 
 #endif

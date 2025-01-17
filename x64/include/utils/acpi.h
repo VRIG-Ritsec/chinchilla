ffc7485c0edee4822106926361dfe6691fa27978
Eddie Xiao <thy.isnis@gmail.com>
2025-01-17 11:04:21 -0500

    FUCKING TYPES for acpi.h

--- x64/include/utils/acpi.h
+++ x64/include/utils/acpi.h
@@ -29,14 +29,14 @@ struct XSDP_t {
 
 struct ACPISDTHeader {
   char Signature[4];
-  uint32_t Length;
-  uint8_t Revision;
-  uint8_t Checksum;
+  u32 Length;
+  u8 Revision;
+  u8 Checksum;
   char OEMID[6];
   char OEMTableID[8];
-  uint32_t OEMRevision;
-  uint32_t CreatorID;
-  uint32_t CreatorRevision;
+  u32 OEMRevision;
+  u32 CreatorID;
+  u32 CreatorRevision;
 };
 
 struct RSDT {
@@ -44,8 +44,83 @@ struct RSDT {
   u32 SDT_pointers[];
 };
 
+struct lame_address_struct
+{
+  u8 AddressSpace;
+  u8 BitWidth;
+  u8 BitOffset;
+  u8 AccessSize;
+  u64 Address;
+};
 
-u64 find_rsdp();
-u64 find_fadt();
+struct FADT
+{
+    struct   ACPISDTHeader h;
+    u32 FirmwareCtrl;
+    u32 Dsdt;
 
+    // field used in ACPI 1.0; no longer in use, for compatibility only
+    u8  Reserved;
+
+    u8  PreferredPowerManagementProfile;
+    u16 SCI_Interrupt;
+    u32 SMI_CommandPort;
+    u8  AcpiEnable;
+    u8  AcpiDisable;
+    u8  S4BIOS_REQ;
+    u8  PSTATE_Control;
+    u32 PM1aEventBlock;
+    u32 PM1bEventBlock;
+    u32 PM1aControlBlock;
+    u32 PM1bControlBlock;
+    u32 PM2ControlBlock;
+    u32 PMTimerBlock;
+    u32 GPE0Block;
+    u32 GPE1Block;
+    u8  PM1EventLength;
+    u8  PM1ControlLength;
+    u8  PM2ControlLength;
+    u8  PMTimerLength;
+    u8  GPE0Length;
+    u8  GPE1Length;
+    u8  GPE1Base;
+    u8  CStateControl;
+    u16 WorstC2Latency;
+    u16 WorstC3Latency;
+    u16 FlushSize;
+    u16 FlushStride;
+    u8  DutyOffset;
+    u8  DutyWidth;
+    u8  DayAlarm;
+    u8  MonthAlarm;
+    u8  Century;
+
+    // reserved in ACPI 1.0; used since ACPI 2.0+
+    u16 BootArchitectureFlags;
+
+    u8  Reserved2;
+    u32 Flags;
+
+    // 12 byte structure; see below for details
+    struct lame_address_struct ResetReg;
+
+    u8  ResetValue;
+    u8  Reserved3[3];
+  
+    // 64bit pointers - Available on ACPI 2.0+
+    uint64_t                X_FirmwareControl;
+    uint64_t                X_Dsdt;
+
+    struct lame_address_struct X_PM1aEventBlock;
+    struct lame_address_struct X_PM1bEventBlock;
+    struct lame_address_struct X_PM1aControlBlock;
+    struct lame_address_struct X_PM1bControlBlock;
+    struct lame_address_struct X_PM2ControlBlock;
+    struct lame_address_struct X_PMTimerBlock;
+    struct lame_address_struct X_GPE0Block;
+    struct lame_address_struct X_GPE1Block;
+};
+bool enable_acpi_mode();
+extern void outb(u8, u8);
+extern u8 inw(u8);
 #endif

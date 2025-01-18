23689e4e131282e28bf93358d7164a739bfb37ec
Eddie Xiao <thy.isnis@gmail.com>
2025-01-17 23:49:29 -0500

    Formating again

--- x64/include/utils/acpi.h
+++ x64/include/utils/acpi.h
@@ -1,74 +1,72 @@
 #ifndef __ACPI_H
 #define __ACPI_H
 
-#include "utils/types.h"
 #include "utils/assert.h"
-#include "utils/string.h"
 #include "utils/printf.h"
+#include "utils/string.h"
+#include "utils/types.h"
 
 struct RSDP_t {
- char Signature[8];
- u8 Checksum;
- char OEMID[6];
- u8 Revision;
- u32 RsdtAddress;
-} __attribute__ ((packed));
+    char Signature[8];
+    u8 Checksum;
+    char OEMID[6];
+    u8 Revision;
+    u32 RsdtAddress;
+} __attribute__((packed));
 
 struct XSDP_t {
- char Signature[8];
- u8 Checksum;
- char OEMID[6];
- u8 Revision;
- u32 RsdtAddress;      // deprecated since version 2.0
+    char Signature[8];
+    u8 Checksum;
+    char OEMID[6];
+    u8 Revision;
+    u32 RsdtAddress; // deprecated since version 2.0
 
- u32 Length;
- u64 XsdtAddress;
- u8 ExtendedChecksum;
- u8 reserved[3];
-} __attribute__ ((packed));
+    u32 Length;
+    u64 XsdtAddress;
+    u8 ExtendedChecksum;
+    u8 reserved[3];
+} __attribute__((packed));
 
 struct ACPISDTHeader {
-  char Signature[4];
-  u32 Length;
-  u8 Revision;
-  u8 Checksum;
-  char OEMID[6];
-  char OEMTableID[8];
-  u32 OEMRevision;
-  u32 CreatorID;
-  u32 CreatorRevision;
+    char Signature[4];
+    u32 Length;
+    u8 Revision;
+    u8 Checksum;
+    char OEMID[6];
+    char OEMTableID[8];
+    u32 OEMRevision;
+    u32 CreatorID;
+    u32 CreatorRevision;
 };
 
 struct RSDT {
-  struct ACPISDTHeader header;
-  u32 SDT_pointers[];
+    struct ACPISDTHeader header;
+    u32 SDT_pointers[];
 };
 
-struct lame_address_struct
-{
-  u8 AddressSpace;
-  u8 BitWidth;
-  u8 BitOffset;
-  u8 AccessSize;
-  u64 Address;
+struct lame_address_struct {
+    u8 AddressSpace;
+    u8 BitWidth;
+    u8 BitOffset;
+    u8 AccessSize;
+    u64 Address;
 };
 
-struct FADT
-{
-    struct   ACPISDTHeader h;
+struct FADT {
+    struct ACPISDTHeader h;
     u32 FirmwareCtrl;
     u32 Dsdt;
 
     // field used in ACPI 1.0; no longer in use, for compatibility only
-    u8  Reserved;
+    u8 Reserved;
 
-    u8  PreferredPowerManagementProfile;
+    u8 PreferredPowerManagementProfile;
     u16 SCI_Interrupt;
     u32 SMI_CommandPort;
-    u8  AcpiEnable;
-    u8  AcpiDisable;
-    u8  S4BIOS_REQ;
-    u8  PSTATE_Control;
+    u8 AcpiEnable;
+    u8 AcpiDisable;
+    u8 S4BIOS_REQ;
+    u8 PSTATE_Control;
     u32 PM1aEventBlock;
     u32 PM1bEventBlock;
     u32 PM1aControlBlock;
@@ -77,39 +75,39 @@ struct FADT
     u32 PMTimerBlock;
     u32 GPE0Block;
     u32 GPE1Block;
-    u8  PM1EventLength;
-    u8  PM1ControlLength;
-    u8  PM2ControlLength;
-    u8  PMTimerLength;
-    u8  GPE0Length;
-    u8  GPE1Length;
-    u8  GPE1Base;
-    u8  CStateControl;
+    u8 PM1EventLength;
+    u8 PM1ControlLength;
+    u8 PM2ControlLength;
+    u8 PMTimerLength;
+    u8 GPE0Length;
+    u8 GPE1Length;
+    u8 GPE1Base;
+    u8 CStateControl;
     u16 WorstC2Latency;
     u16 WorstC3Latency;
     u16 FlushSize;
     u16 FlushStride;
-    u8  DutyOffset;
-    u8  DutyWidth;
-    u8  DayAlarm;
-    u8  MonthAlarm;
-    u8  Century;
+    u8 DutyOffset;
+    u8 DutyWidth;
+    u8 DayAlarm;
+    u8 MonthAlarm;
+    u8 Century;
 
     // reserved in ACPI 1.0; used since ACPI 2.0+
     u16 BootArchitectureFlags;
 
-    u8  Reserved2;
+    u8 Reserved2;
     u32 Flags;
 
     // 12 byte structure; see below for details
     struct lame_address_struct ResetReg;
 
-    u8  ResetValue;
-    u8  Reserved3[3];
-  
+    u8 ResetValue;
+    u8 Reserved3[3];
+
     // 64bit pointers - Available on ACPI 2.0+
-    uint64_t                X_FirmwareControl;
-    uint64_t                X_Dsdt;
+    uint64_t X_FirmwareControl;
+    uint64_t X_Dsdt;
 
     struct lame_address_struct X_PM1aEventBlock;
     struct lame_address_struct X_PM1bEventBlock;
--- x64/src/utils/acpi.c
+++ x64/src/utils/acpi.c
@@ -6,34 +6,32 @@
 #define RSDP_HEADER "RSD PTR "
 #define RSDP_HEADER_LENGTH (sizeof(RSDP_HEADER) - 1)
 #define FADT_HEADER "FACP"
-#define FADT_HEADER_LENGTH (sizeof(FADT_HEADER)-1) 
-
+#define FADT_HEADER_LENGTH (sizeof(FADT_HEADER) - 1)
 
 // https://wiki.osdev.org/RSDP#Checksum_validation
-bool rsdp_checksum(struct RSDP_t* rsdp){
+bool rsdp_checksum(struct RSDP_t *rsdp) {
     u8 sum = 0;
-    for(u32 i = 0; i < sizeof(struct RSDP_t); i ++){
+    for (u32 i = 0; i < sizeof(struct RSDP_t); i++) {
         // Add up each byte and make sure lowest byte equal zero
-        sum += ((u8*)rsdp)[i];
+        sum += ((u8 *)rsdp)[i];
     }
     return sum == 0;
 }
 
-bool xsdp_checksum(struct XSDP_t* xsdp){
+bool xsdp_checksum(struct XSDP_t *xsdp) {
     u8 sum = 0;
-    for(u32 i = 0; i < sizeof(struct XSDP_t); i ++){
+    for (u32 i = 0; i < sizeof(struct XSDP_t); i++) {
         // Add up each byte and make sure lowest byte equal zero
-        sum += ((u8*)xsdp)[i];
+        sum += ((u8 *)xsdp)[i];
     }
     return sum == 0;
 }
-
 
 // Technique used https://wiki.osdev.org/RSDP#Detecting_the_RSDP
-u64 find_rsdp(){
+u64 find_rsdp() {
     // look in the first kb of EBDA + Header is 16 byte aligned
-    for(u32 i = 0; i < 0x400; i+= 0x10){
-        if(!memcmp((void*)EBDA_START + i, RSDP_HEADER, RSDP_HEADER_LENGTH)){
+    for (u32 i = 0; i < 0x400; i += 0x10) {
+        if (!memcmp((void *)EBDA_START + i, RSDP_HEADER, RSDP_HEADER_LENGTH)) {
             // found the string
             return EBDA_START + i;
         }
@@ -41,12 +39,11 @@ u64 find_rsdp(){
 
     // Look in main BIOS area under 1MB
     u64 bios_start = 0xE0000;
-    u64 bios_size = 128 * KILOBTYE;    // 128 kb
-
+    u64 bios_size = 128 * KILOBTYE; // 128 kb
 
     // look in the 128 kb of bios region + Header is 16 byte aligned
-    for(u32 i = 0; i < bios_size; i += 0x10){
-        if(!memcmp((void*)bios_start+ i, RSDP_HEADER, RSDP_HEADER_LENGTH)){
+    for (u32 i = 0; i < bios_size; i += 0x10) {
+        if (!memcmp((void *)bios_start + i, RSDP_HEADER, RSDP_HEADER_LENGTH)) {
             // found the string
             return bios_start + i;
         }
@@ -54,37 +51,39 @@ u64 find_rsdp(){
     return 0;
 }
 
-struct FADT * find_fadt(){
-    struct RSDP_t * rsdp = (struct RSDP_t*)find_rsdp();
+struct FADT *find_fadt() {
+    struct RSDP_t *rsdp = (struct RSDP_t *)find_rsdp();
     // version 1 aka rsdp
-    if(rsdp->Revision == 0){
+    if (rsdp->Revision == 0) {
         ASSERT(!rsdp_checksum(rsdp), "RSDP invalid checksum");
-    }else{
-        // other versions, cast rsdp into xsdp 
-        struct XSDP_t * xsdp = (struct XSDP_t*)rsdp;
+    } else {
+        // other versions, cast rsdp into xsdp
+        struct XSDP_t *xsdp = (struct XSDP_t *)rsdp;
         ASSERT(!xsdp_checksum(xsdp), "XSDP invalid checksum");
     }
 
-    struct RSDT * rsdt = (struct RSDT*)(u64)rsdp->RsdtAddress;
+    struct RSDT *rsdt = (struct RSDT *)(u64)rsdp->RsdtAddress;
 
     u32 entries = (rsdt->header.Length - sizeof(struct ACPISDTHeader)) / 4;
-    for(u32 i = 0; i < entries; i++){
-        struct ACPISDTHeader *entry = (struct ACPISDTHeader *)(u64)rsdt->SDT_pointers[i];
-        if(!memcmp(entry->Signature, FADT_HEADER, FADT_HEADER_LENGTH)){
+    for (u32 i = 0; i < entries; i++) {
+        struct ACPISDTHeader *entry =
+            (struct ACPISDTHeader *)(u64)rsdt->SDT_pointers[i];
+        if (!memcmp(entry->Signature, FADT_HEADER, FADT_HEADER_LENGTH)) {
             /*PINFO("FOUND FACP: %#lx\n", entry);*/
-            return (struct FADT*)entry;
+            return (struct FADT *)entry;
         }
     }
     return NULL;
 }
 
-bool enable_acpi_mode(){
+bool enable_acpi_mode() {
     // https://wiki.osdev.org/ACPI#Switching_to_ACPI_Mode
-    struct FADT * fadt = find_fadt();
+    struct FADT *fadt = find_fadt();
     ASSERT(fadt == NULL, "FADT NOT FOUND\n");
     // write acpi_enable to SMI command field
     outb(fadt->SMI_CommandPort, fadt->AcpiEnable);
-    while ((inw(fadt->PM1aControlBlock) & 1) == 0);
+    while ((inw(fadt->PM1aControlBlock) & 1) == 0)
+        ;
     PINFO("ACPI Initialized Successfully\n");
     return 1;
 }
